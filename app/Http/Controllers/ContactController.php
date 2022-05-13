d1f5c163e4d57d0dd368ede868af88cab2591755
Misha-i <67232525+Misha-i@users.noreply.github.com>
2022-05-13 09:49:52 +0300

    Видалив коментарій.

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,64 +9,6 @@ use Illuminate\Pagination\Paginator;
 
 class ContactController extends Controller
 {
-    /*public function index(Request $request)
-    {
-        $contact = new Contact();
-        Paginator::useBootstrap();
-        return view('newDataPeople.home' ['data', => Contact::paginate()]);
-    }*/
-
-    /*public function submit(ContactRequest $request) {
-        $contact = new Contact();
-        $contact->name=$request->input('name');
-        $contact->email=$request->input('email');
-        $contact->subject=$request->input('subject');
-        $contact->message=$request->input('message');
-        // Передаємо в екземпляр класу в таблицю name_cars даті з форми яка прийшла по реквесту,
-
-        $contact->save();
-        // Зберігаємо
-
-        return redirect()->route('table1')->with('success', 'Повідомлення було відправленно');
-        // повертаємо сторінку по роуті table з повідомленням
-    }
-
-    public function create()
-    {
-        return view('NewDataPeople.contact');
-    }*/
-
-    /*public function showOneCar($id) {
-        $contact= Contact::where('id', $id)->first();
-        return view('NewDataPeople.one-message', ['data' => $contact]);
-    }
-    public function edit(Contact $contact){
-
-    }
-
-    public function updateMessage($id) {
-        $contact = new Contact();
-        $contact = Contact::where('id', $id)->first();
-        return view('newProject.update-message', ['data' => $contact]);
-    }
-
-    public function updateMessageSubmit($id, ContactRequest $request) {
-
-        $contact = Contact::find($id);
-        $contact->name=$request->input('name');
-        $contact->email=$request->input('email');
-        $contact->subject=$request->input('subject');
-        $contact->message=$request->input('message');
-
-        $contact->save();
-        return redirect()->route('car-all', $id)->with('success', 'Повідомлення було оновленно');
-    }
-
-    public function deleteMessageSubmit($id){
-        Contact::find($id)->delete();
-        return redirect()->route('table')->with('success', 'Повідомлення було видалено');
-    }*/
-
     public function home(){
         return view('NewDataPeople.home');
     }
@@ -88,8 +30,6 @@ class ContactController extends Controller
         $contact->email=$request->input('email');
         $contact->subject=$request->input('subject');
         $contact->message=$request->input('message');
-
-
         $contact->save();
 
         return redirect()->route('table1')->with('success', 'Повідомлення було відправленно');
@@ -111,7 +51,6 @@ class ContactController extends Controller
         $contact->email=$request->input('email');
         $contact->subject=$request->input('subject');
         $contact->message=$request->input('message');
-
         $contact->save();
 
         return redirect()->route('table1')->with('success', 'Повідомлення було оновлено');
