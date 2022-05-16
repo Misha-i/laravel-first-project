a0d6be2dc8168514cb9d51d3afe6dc3de7897ff2
Misha-i <67232525+Misha-i@users.noreply.github.com>
2022-05-16 14:42:13 +0300

    Видалив файли Contact.php and ContactSeeder.php from folder seeder, і видалив код який не використовувався.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Resources\CarResource;
+use App\Models\Car;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
