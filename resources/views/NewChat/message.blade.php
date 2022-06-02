e849393bc192c4df8661500f5ef18d6b38279982
misha <misha@email.com>
2022-06-02 15:24:34 +0300

    Start learning Fortify

---
+++ resources/views/NewChat/message.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Online Chat</title>
+</head>
+<body>
+
+<form action=”mailto:contact@yourdomain.com”
+      method=”POST”
+      enctype=”multipart/form-data”
+      name=”EmailForm”>
+    Name:<br>
+    <input type=”text” size=”19″ name=”ContactName”><br><br>
+    Message:<br> <textarea name=”ContactCommentt” rows=”6″ cols=”20″>
+</textarea><br><br> <input type=”submit” value=”Submit”> </form>
+
+</body>
+</html>
